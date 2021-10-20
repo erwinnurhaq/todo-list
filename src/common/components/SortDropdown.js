4877a0bfc8c35f3db9f9b878d2824c7b2234018c
Erwin Nurhaq <simkalastoforka@gmail.com>
2021-10-21 01:37:46 +0700

    fix

--- src/common/components/SortDropdown.js
+++ src/common/components/SortDropdown.js
@@ -2,11 +2,6 @@ import { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import Dropdown from 'bootstrap/js/dist/dropdown';
 
-import { ReactComponent as NewestIcon } from 'assets/icons/icon-sort-newest.svg';
-import { ReactComponent as OldestIcon } from 'assets/icons/icon-sort-oldest.svg';
-import { ReactComponent as AscIcon } from 'assets/icons/icon-sort-asc.svg';
-import { ReactComponent as DescIcon } from 'assets/icons/icon-sort-desc.svg';
-import { ReactComponent as NotDoneIcon } from 'assets/icons/icon-sort-blue.svg';
 import { ReactComponent as SortIcon } from 'assets/icons/icon-sort.svg';
 import { ReactComponent as CheckIcon } from 'assets/icons/icon-check.svg';
 import { SORT } from 'common/constants/activity';
@@ -14,23 +9,6 @@ import { SORT } from 'common/constants/activity';
 function SortDropdown({ value, onChange }) {
   const dropdownRef = useRef(null);
   const dropdown = useRef(null);
-
-  const RenderIcon = type => {
-    switch (type) {
-      case 'NEWEST':
-        return <NewestIcon />;
-      case 'OLDEST':
-        return <OldestIcon />;
-      case 'ASC':
-        return <AscIcon />;
-      case 'DESC':
-        return <DescIcon />;
-      case 'NOTDONE':
-        return <NotDoneIcon />;
-      default:
-        return <NotDoneIcon />;
-    }
-  };
 
   useEffect(() => {
     dropdown.current = new Dropdown(dropdownRef.current);
@@ -57,7 +35,7 @@ function SortDropdown({ value, onChange }) {
               onClick={() => onChange(SORT[key])}
               data-cy={SORT[key] === value && 'sort-selection-selected'}
             >
-              <div data-cy="sort-selection-icon">{RenderIcon(key)}</div>
+              <div data-cy="sort-selection-icon">o</div>
               <p data-cy="sort-selection-title">{SORT[key]}</p>
               {SORT[key] === value && <CheckIcon />}
             </button>
