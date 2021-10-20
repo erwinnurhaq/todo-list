0647dbd3d2c4f823cd3a014ba4831167c908bb0d
Erwin Nurhaq <simkalastoforka@gmail.com>
2021-10-20 21:59:30 +0700

    move notes

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom';
 import './styles/index.scss';
 import App from './routers';
 
-console.log(process.env.NOTES || 'Jangan Maling Oi!');
-
 ReactDOM.render(
   <React.StrictMode>
+    {!process.env.REACT_APP_VER_NOTES && <h1>Hayo maling hayo...</h1>}
     <App />
   </React.StrictMode>,
   document.getElementById('root')
