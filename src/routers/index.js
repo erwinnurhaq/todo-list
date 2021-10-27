4972d99c30dbe1929f06e01a1e80d28a0bc212ba
Erwin Nurhaq <simkalastoforka@gmail.com>
2021-10-27 19:41:40 +0700

    deploy to gh pages

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 
 import Layout from '../layouts';
 import Activity from '../pages/Activity';
 import ActivityDetail from '../pages/ActivityDetail';
 
 const App = () => (
-  <BrowserRouter>
+  <HashRouter basename="/">
     <Layout>
       <Switch>
         <Route exact path="/" component={Activity} />
         <Route exact path="/detail/:id" component={ActivityDetail} />
       </Switch>
     </Layout>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default App;
